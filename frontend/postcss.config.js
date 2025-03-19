309af0a480a2d7c6955bd6e1878578be88c80b2c
Marcello <marcello.castano@hotmail.it>
2025-03-19 22:25:14 +0100

    tailwind

--- frontend/postcss.config.js
+++ frontend/postcss.config.js
@@ -1,11 +1,16 @@
+import tailwindcss from 'tailwindcss';
+import tailwindConfig from './tailwind.config.js';
+import autoprefixer from 'autoprefixer';
+import postcssNested from 'postcss-nested';
+
 export default {
   plugins: {
     // Aggiungi il plugin per il nesting CSS prima di Tailwind
-    'postcss-nested': {},
+    'postcss-nested': postcssNested,
     // Il plugin di nesting di Tailwind deve venire dopo postcss-nested ma prima di tailwindcss
     'tailwindcss/nesting': 'postcss-nested',
-    tailwindcss: {},
-    autoprefixer: {},
+    tailwindcss: tailwindConfig,
+    autoprefixer: autoprefixer,
     ...(process.env.NODE_ENV === 'production' ? {
       cssnano: {
         preset: ['default', {
